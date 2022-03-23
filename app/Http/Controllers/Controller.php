11ab77ad0a2fcf29d709a10073ffb5e44ffb3177
Mraz Tevin <millatevin@gmail.com>
2022-03-23 12:02:55 +0300

    create controller functions

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,10 +2,40 @@
 
 namespace App\Http\Controllers;
 
+
 use Laravel\Lumen\Routing\Controller as BaseController;
 use App\Models\Comment;
 
 class Controller extends BaseController
 {
-    //
+    public function index() 
+    {
+        $comments = Comment::all();
+ 
+        return response()->json($comments);
+    }
+
+    public function show($id)
+    {
+        // show 1 comment
+        $comment = Comment::find($id);
+
+        return response()->json($comment);
+    }
+
+    public function store(Request $request)
+    {
+        // post user comment to database
+         $this->validate($request,[
+            'comment' => 'required',
+            // 'created at',
+            // 'visitor',
+         ]);
+    }
+    public function destroy($id)
+    {
+        $comment = Comment::find($id);
+        $comment->delete();
+        return response()->json('Successfully deleted comment!');
+    }
 }
