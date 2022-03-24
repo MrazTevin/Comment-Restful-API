6f411ec40a49359dc746bcfe0902e728f05f595f
Mraz Tevin <millatevin@gmail.com>
2022-03-24 22:29:55 +0300

    redo migrations

--- database/migrations/2022_03_23_075229_create_comments_table.php
+++ database/migrations/2022_03_23_075229_create_comments_table.php
@@ -12,13 +12,15 @@ return new class extends Migration
      * @return void
      */
     public function up()
+    // creates comment table
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->engine='InnoDB';
             $table->bigIncrements('id');
+            $table->string('bookid',8);
             $table->text('comment',500);
-            $table->timestamp('created_at')->useCurrent();
-            $table->ipAddress('visitor')->nullable();   
+            $table->timestamp('created_at',6)->useCurrent();
+            $table->ipAddress('visitor',45)->nullable();   
             // $table->timestamps();
         });
     }
