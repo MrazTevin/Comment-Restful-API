b9b6c5d55edf48a6251ca218fa6908a4a49d98cd
Mraz Tevin <millatevin@gmail.com>
2022-03-24 23:11:08 +0300

    function that returns users ip address

--- app/Traits/PickIpTrait.php
+++ app/Traits/PickIpTrait.php
@@ -5,4 +5,19 @@ namespace   App\Traits;
 class PickIpTrait 
 {
     private $ipAddress = null;
+
+
+    public function getClientIp()
+    {
+        if(!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            $ipAddress = $_SERVER['HTTP_CLIENT_IP'];  
+        } elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ipAddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            $ipAddress=$_SERVER['REMOTE_ADDR'];
+        }
+
+        return $ipAddress;
+    }
+  
 }
