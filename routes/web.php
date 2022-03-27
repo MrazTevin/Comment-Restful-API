03257b525dc634d5cd60fdcb67b3bdd9131e2e61
Mraz Tevin <millatevin@gmail.com>
2022-03-27 07:06:57 +0300

    modify route prefix

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,10 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->group(['prefix' => 'api'], function () use ($router) {
+$router->group(['prefix' => 'api/v1'], function () use ($router) {
     $router->get('/comments',  ['uses' => 'CommentController@showAllComments']);
     $router->get('/comments/{id}', ['uses' => 'CommentController@showOneComment']);
-    $router->post('/comments', ['uses' => 'CommentController@store']);
+    $router->post('/comments', ['uses' => 'CommentController@create ']);
     $router->put('/comments/{id}', ['uses' => 'CommentController@update']);
     $router->delete('/comments/{id}', ['uses' => 'CommentController@destroy']);
 });
