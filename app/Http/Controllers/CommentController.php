71daed5608413421eb57725374fd0eb6bf5dab58
Mraz Tevin <millatevin@gmail.com>
2022-03-27 09:35:22 +0300

    fetch and store ip address

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Comment;
+use App\Traits\PickIpTrait;
 
 class CommentController extends Controller
 {
@@ -29,6 +30,7 @@ class CommentController extends Controller
         
         //and add the comment to database
         $comment = new Comment();
+        $ipAddress = new PickIpTrait();
         $comment->comment = $request->input('comment');
         $comment->bookid = $request->input('bookid');
 
