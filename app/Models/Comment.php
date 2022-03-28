7558785233c0c9b7b6690983265ccaeb691cbd34
Mraz Tevin <millatevin@gmail.com>
2022-03-28 15:24:18 +0300

    modify models

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     protected $fillable = [
-        'comment','bookid'
+        'bookid','comment','visitor','date_added'
     ];
-
+    protected $hidden = ['created_at', 'updated_at'];
     protected $table = 'comments';
+
 }
